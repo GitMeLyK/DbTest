fb134ba5b00d2cc215469bdab94d2d71392c5cf6
unknown <unknown>
2023-08-18 15:01:43 +0200

    Initiasl

---
+++ VersionSQL/WIN-SRV-22-DATA/TEMP_DB_TESTING/dbo/Table/TABLE_TEST.sql
@@ -0,0 +1,9 @@
+﻿/****** Object:  Table [dbo].[TABLE_TEST]    Committed by VersionSQL https://www.versionsql.com ******/
+
+SET ANSI_NULLS ON
+SET QUOTED_IDENTIFIER ON
+CREATE TABLE [dbo].[TABLE_TEST](
+	[ID] [int] IDENTITY(0,1) NOT NULL,
+	[NAME] [char](50) NOT NULL,
+	[DESC] [nchar](50) NULL
+) ON [PRIMARY]
